2ce22272e5ee4a13387e21d2c23fe85695ee6db3
Alice Cecile <alice.i.cecile@gmail.com>
2021-08-24 12:02:26 -0400

    Fix tile-map texture size

--- examples/map.rs
+++ examples/map.rs
@@ -25,7 +25,7 @@ fn startup(
             UVec2::new(2, 2),
             UVec2::new(8, 8),
             Vec2::new(16.0, 16.0),
-            Vec2::new(96.0, 256.0),
+            Vec2::new(96.0, 16.0),
         ),
         0u16,
         0u16,
