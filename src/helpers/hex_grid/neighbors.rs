d5d31bb67da588402aa161dba35c5927807c507f
Brian Merchant <bhmerchang@gmail.com>
2022-09-23 10:00:06 -0700

    Simplification.

--- src/helpers/hex_grid/neighbors.rs
+++ src/helpers/hex_grid/neighbors.rs
@@ -1,7 +1,139 @@
 use crate::helpers::hex_grid::axial::AxialPos;
 use std::ops::{Add, Sub};
 
-/// Compass neighbors of a tile in hexagonal row-oriented coordinate systems
+/// Neighbors of a hexagonal tile. `Zero` corresponds with `East` for row-oriented tiles, and
+/// `North` for column-oriented tiles. It might also correspond with a custom direction for
+/// hex maps which are oriented by a non-standard rotation.
+///
+/// Mathematically: `Zero` corresponds with "zero multiples of `pi/3`", `One` with "one multiple of
+/// `pi/3`", and so on.
+///
+/// [`HexDirection`]s can be added, and subtracted (under the hood, it is addition/subtraction
+/// modulo 6).
+///
+/// [`HexDirection`]s can be converted from/into `usize`, `u32`, `isize`, `i32`.
+#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq)]
+pub enum HexDirection {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+}
+
+/// Array of [`HexDirection`] variants.
+pub const HEX_DIRECTIONS: [HexDirection; 6] = [
+    HexDirection::Zero,
+    HexDirection::One,
+    HexDirection::Two,
+    HexDirection::Three,
+    HexDirection::Four,
+    HexDirection::Five,
+];
+
+/// Offsets of tiles that lie in each [`HexDirection`].
+pub const HEX_OFFSETS: [AxialPos; 6] = [
+    AxialPos { q: 1, r: 0 },
+    AxialPos { q: 0, r: 1 },
+    AxialPos { q: -1, r: 1 },
+    AxialPos { q: -1, r: 0 },
+    AxialPos { q: 0, r: -1 },
+    AxialPos { q: 1, r: -1 },
+];
+
+impl From<usize> for HexDirection {
+    fn from(choice: usize) -> Self {
+        let ix = choice % 6;
+        HEX_DIRECTIONS[ix]
+    }
+}
+
+impl From<isize> for HexDirection {
+    fn from(choice: isize) -> Self {
+        // The Euclidean remainder is always positive, so it is safe to convert to usize;
+        let ix = choice.rem_euclid(6) as usize;
+        HEX_DIRECTIONS[ix]
+    }
+}
+
+impl From<u32> for HexDirection {
+    fn from(choice: u32) -> Self {
+        (choice as usize).into()
+    }
+}
+
+impl From<i32> for HexDirection {
+    fn from(choice: i32) -> Self {
+        (choice as isize).into()
+    }
+}
+
+impl Add<usize> for HexDirection {
+    type Output = HexDirection;
+
+    fn add(self, rhs: usize) -> Self::Output {
+        ((self as usize) + rhs).into()
+    }
+}
+
+impl Add<u32> for HexDirection {
+    type Output = HexDirection;
+
+    fn add(self, rhs: u32) -> Self::Output {
+        ((self as usize) + rhs as usize).into()
+    }
+}
+
+impl Add<isize> for HexDirection {
+    type Output = HexDirection;
+
+    fn add(self, rhs: isize) -> Self::Output {
+        ((self as isize) + rhs).into()
+    }
+}
+
+impl Add<i32> for HexDirection {
+    type Output = HexDirection;
+
+    fn add(self, rhs: i32) -> Self::Output {
+        ((self as i32) + rhs).into()
+    }
+}
+
+impl Sub<usize> for HexDirection {
+    type Output = HexDirection;
+
+    fn sub(self, rhs: usize) -> Self::Output {
+        ((self as usize) - rhs).into()
+    }
+}
+
+impl Sub<u32> for HexDirection {
+    type Output = HexDirection;
+
+    fn sub(self, rhs: u32) -> Self::Output {
+        ((self as usize) - rhs as usize).into()
+    }
+}
+
+impl Sub<isize> for HexDirection {
+    type Output = HexDirection;
+
+    fn sub(self, rhs: isize) -> Self::Output {
+        ((self as isize) - rhs).into()
+    }
+}
+
+impl Sub<i32> for HexDirection {
+    type Output = HexDirection;
+
+    fn sub(self, rhs: i32) -> Self::Output {
+        ((self as i32) - rhs).into()
+    }
+}
+
+/// Compass directions of a tile in hexagonal row-oriented coordinate systems
 /// ([Row](crate::map::HexCoordSystem::Row), [RowEven](crate::map::HexCoordSystem::RowEven), and
 /// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
 #[derive(Clone, Copy, Debug, PartialOrd, Ord, Eq, PartialEq, Hash)]
@@ -14,156 +146,7 @@ pub enum HexRowDirection {
     SouthEast,
 }
 
-pub const HEX_ROW_DIRECTIONS: [HexRowDirection; 6] = [
-    HexRowDirection::East,
-    HexRowDirection::NorthEast,
-    HexRowDirection::NorthWest,
-    HexRowDirection::West,
-    HexRowDirection::SouthWest,
-    HexRowDirection::SouthEast,
-];
-
-impl From<usize> for HexRowDirection {
-    fn from(choice: usize) -> Self {
-        let ix = choice % 6;
-        HEX_ROW_DIRECTIONS[ix]
-    }
-}
-
-impl From<isize> for HexRowDirection {
-    fn from(choice: isize) -> Self {
-        // The Euclidean remainder is always positive, so it is safe to convert to usize;
-        let ix = choice.rem_euclid(6) as usize;
-        HEX_ROW_DIRECTIONS[ix]
-    }
-}
-
-impl From<u32> for HexRowDirection {
-    fn from(choice: u32) -> Self {
-        (choice as usize).into()
-    }
-}
-
-impl From<i32> for HexRowDirection {
-    fn from(choice: i32) -> Self {
-        (choice as isize).into()
-    }
-}
-
-impl Add<usize> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn add(self, rhs: usize) -> Self::Output {
-        ((self as usize) + rhs).into()
-    }
-}
-
-impl Add<u32> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn add(self, rhs: u32) -> Self::Output {
-        ((self as usize) + rhs as usize).into()
-    }
-}
-
-impl Add<isize> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn add(self, rhs: isize) -> Self::Output {
-        ((self as isize) + rhs).into()
-    }
-}
-
-impl Add<i32> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn add(self, rhs: i32) -> Self::Output {
-        ((self as i32) + rhs).into()
-    }
-}
-
-impl Sub<usize> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn sub(self, rhs: usize) -> Self::Output {
-        ((self as usize) - rhs).into()
-    }
-}
-
-impl Sub<u32> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn sub(self, rhs: u32) -> Self::Output {
-        ((self as usize) - rhs as usize).into()
-    }
-}
-
-impl Sub<isize> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn sub(self, rhs: isize) -> Self::Output {
-        ((self as isize) - rhs).into()
-    }
-}
-
-impl Sub<i32> for HexRowDirection {
-    type Output = HexRowDirection;
-
-    fn sub(self, rhs: i32) -> Self::Output {
-        ((self as i32) - rhs).into()
-    }
-}
-
-/// Vectors associated with compass directions of neighboring tiles in hexagonal row-oriented
-/// coordinate systems ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_DIRECTIONS: [AxialPos; 6] = [
-    AXIAL_ROW_EAST,
-    AXIAL_ROW_NORTH_EAST,
-    AXIAL_ROW_NORTH_WEST,
-    AXIAL_ROW_WEST,
-    AXIAL_ROW_SOUTH_WEST,
-    AXIAL_ROW_SOUTH_EAST,
-];
-
-/// Vector associated with East in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_EAST: AxialPos = AxialPos { q: 1, r: 0 };
-
-/// Vector associated with East in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_NORTH_EAST: AxialPos = AxialPos { q: 0, r: 1 };
-
-/// Vector associated with North-West in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_NORTH_WEST: AxialPos = AxialPos { q: -1, r: 1 };
-
-/// Vector associated with West in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_WEST: AxialPos = AxialPos { q: -1, r: 0 };
-
-/// Vector associated with South-West in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_SOUTH_WEST: AxialPos = AxialPos { q: 0, r: -1 };
-
-/// Vector associated with South-East in hexagonal row-oriented coordinate systems
-/// ([Row](crate::map::HexCoordSystem::Row),
-/// [RowEven](crate::map::HexCoordSystem::RowEven), and
-/// [RowOdd](crate::map::HexCoordSystem::RowOdd)).
-pub const AXIAL_ROW_SOUTH_EAST: AxialPos = AxialPos { q: 1, r: -1 };
-
-/// Compass neighbors of a tile in hexagonal column-oriented coordinate systems
+/// Compass directions of a tile in hexagonal column-oriented coordinate systems
 /// ([Column](crate::map::HexCoordSystem::Column),
 /// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
 /// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
@@ -177,151 +160,44 @@ pub enum HexColDirection {
     NorthEast,
 }
 
-pub const HEX_COL_DIRECTIONS: [HexColDirection; 6] = [
-    HexColDirection::North,
-    HexColDirection::NorthWest,
-    HexColDirection::SouthWest,
-    HexColDirection::South,
-    HexColDirection::SouthEast,
-    HexColDirection::NorthEast,
-];
-
-impl From<usize> for HexColDirection {
-    fn from(choice: usize) -> Self {
-        let ix = choice % 6;
-        HEX_COL_DIRECTIONS[ix]
+impl From<HexDirection> for HexRowDirection {
+    fn from(direction: HexDirection) -> Self {
+        use HexDirection::*;
+        use HexRowDirection::*;
+        match direction {
+            Zero => East,
+            One => NorthEast,
+            Two => NorthWest,
+            Three => West,
+            Four => SouthWest,
+            Five => SouthEast,
+        }
     }
 }
 
-impl From<isize> for HexColDirection {
-    fn from(choice: isize) -> Self {
-        // The Euclidean remainder is always positive, so it is safe to convert to usize;
-        let ix = choice.rem_euclid(6) as usize;
-        HEX_COL_DIRECTIONS[ix]
+impl From<HexRowDirection> for HexDirection {
+    fn from(direction: HexRowDirection) -> Self {
+        (direction as usize).into()
     }
 }
 
-impl From<u32> for HexColDirection {
-    fn from(choice: u32) -> Self {
-        (choice as usize).into()
+impl From<HexDirection> for HexColDirection {
+    fn from(direction: HexDirection) -> Self {
+        use HexColDirection::*;
+        use HexDirection::*;
+        match direction {
+            Zero => North,
+            One => NorthWest,
+            Two => SouthWest,
+            Three => South,
+            Four => SouthEast,
+            Five => NorthEast,
+        }
     }
 }
 
-impl From<i32> for HexColDirection {
-    fn from(choice: i32) -> Self {
-        (choice as isize).into()
+impl From<HexColDirection> for HexDirection {
+    fn from(direction: HexColDirection) -> Self {
+        (direction as usize).into()
     }
 }
-
-impl Add<usize> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn add(self, rhs: usize) -> Self::Output {
-        ((self as usize) + rhs).into()
-    }
-}
-
-impl Add<u32> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn add(self, rhs: u32) -> Self::Output {
-        ((self as usize) + rhs as usize).into()
-    }
-}
-
-impl Add<isize> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn add(self, rhs: isize) -> Self::Output {
-        ((self as isize) + rhs).into()
-    }
-}
-
-impl Add<i32> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn add(self, rhs: i32) -> Self::Output {
-        ((self as i32) + rhs).into()
-    }
-}
-
-impl Sub<usize> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn sub(self, rhs: usize) -> Self::Output {
-        ((self as usize) - rhs).into()
-    }
-}
-
-impl Sub<u32> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn sub(self, rhs: u32) -> Self::Output {
-        ((self as usize) - rhs as usize).into()
-    }
-}
-
-impl Sub<isize> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn sub(self, rhs: isize) -> Self::Output {
-        ((self as isize) - rhs).into()
-    }
-}
-
-impl Sub<i32> for HexColDirection {
-    type Output = HexColDirection;
-
-    fn sub(self, rhs: i32) -> Self::Output {
-        ((self as i32) - rhs).into()
-    }
-}
-
-/// Vectors associated with compass directions of neighboring tiles in hexagonal column-oriented
-/// coordinate systems ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_DIRECTIONS: [AxialPos; 6] = [
-    AXIAL_COL_NORTH,
-    AXIAL_COL_NORTH_WEST,
-    AXIAL_COL_SOUTH_WEST,
-    AXIAL_COL_SOUTH,
-    AXIAL_COL_SOUTH_EAST,
-    AXIAL_COL_NORTH_EAST,
-];
-
-/// Vector associated with North-East in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_NORTH_EAST: AxialPos = AxialPos { q: 1, r: 0 };
-
-/// Vector associated with North in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_NORTH: AxialPos = AxialPos { q: 0, r: 1 };
-
-/// Vector associated with West in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_NORTH_WEST: AxialPos = AxialPos { q: -1, r: 1 };
-
-/// Vector associated with South-West in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_SOUTH_WEST: AxialPos = AxialPos { q: -1, r: 0 };
-
-/// Vector associated with South in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_SOUTH: AxialPos = AxialPos { q: 0, r: -1 };
-
-/// Vector associated with South-East in hexagonal column-oriented coordinate systems
-/// ([Column](crate::map::HexCoordSystem::Column),
-/// [ColumnEven](crate::map::HexCoordSystem::ColumnEven), and
-/// [ColumnOdd](crate::map::HexCoordSystem::ColumnOdd)).
-pub const AXIAL_COL_SOUTH_EAST: AxialPos = AxialPos { q: 1, r: -1 };
