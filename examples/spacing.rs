eb119120e2ffae7b6755f32cd49b8f10a0158703
Brian Merchant <bhmerchang@gmail.com>
2022-10-11 16:22:11 -0700

    Update spacing example to use new spawn methods.

--- examples/spacing.rs
+++ examples/spacing.rs
@@ -3,7 +3,7 @@ use bevy_ecs_tilemap::prelude::*;
 mod helpers;
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default());
 
     let texture_handle: Handle<Image> = asset_server.load("tiles-spaced.png");
 
@@ -11,7 +11,7 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
 
     // Layer 1
     let mut tile_storage = TileStorage::empty(tilemap_size);
-    let tilemap_entity = commands.spawn().id();
+    let tilemap_entity = commands.spawn_empty().id();
 
     fill_tilemap(
         TileTexture(0),
@@ -24,22 +24,20 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
     let grid_size = tile_size.into();
 
-    commands
-        .entity(tilemap_entity)
-        .insert_bundle(TilemapBundle {
-            grid_size,
-            size: tilemap_size,
-            storage: tile_storage,
-            texture: TilemapTexture::Single(texture_handle.clone()),
-            tile_size,
-            spacing: TilemapSpacing { x: 8.0, y: 8.0 },
-            transform: get_tilemap_center_transform(&tilemap_size, &grid_size, 0.0),
-            ..Default::default()
-        });
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        size: tilemap_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle.clone()),
+        tile_size,
+        spacing: TilemapSpacing { x: 8.0, y: 8.0 },
+        transform: get_tilemap_center_transform(&tilemap_size, &grid_size, 0.0),
+        ..Default::default()
+    });
 
     // Layer 2
     let mut tile_storage = TileStorage::empty(tilemap_size);
-    let tilemap_entity = commands.spawn().id();
+    let tilemap_entity = commands.spawn_empty().id();
 
     fill_tilemap(
         TileTexture(2),
@@ -49,18 +47,16 @@ fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
         &mut tile_storage,
     );
 
-    commands
-        .entity(tilemap_entity)
-        .insert_bundle(TilemapBundle {
-            grid_size: grid_size,
-            size: tilemap_size,
-            storage: tile_storage,
-            texture: TilemapTexture::Single(texture_handle),
-            tile_size: TilemapTileSize { x: 16.0, y: 16.0 },
-            transform: get_tilemap_center_transform(&tilemap_size, &grid_size, 1.0)
-                * Transform::from_xyz(32.0, 32.0, 0.0),
-            ..Default::default()
-        });
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        size: tilemap_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size: TilemapTileSize { x: 16.0, y: 16.0 },
+        transform: get_tilemap_center_transform(&tilemap_size, &grid_size, 1.0)
+            * Transform::from_xyz(32.0, 32.0, 0.0),
+        ..Default::default()
+    });
 }
 
 fn main() {
