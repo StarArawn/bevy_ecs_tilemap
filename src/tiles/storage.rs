2656937beeb913f9b64413b431ced0460b8ba8ce
Carnagion <indraneel.mahendrakumar@gmail.com>
2023-06-08 20:54:58 +0100

    Add FromReflect impls for other components

--- src/tiles/storage.rs
+++ src/tiles/storage.rs
@@ -6,7 +6,7 @@ use super::TilePos;
 
 /// Used to store tile entities for fast look up.
 /// Tile entities are stored in a grid. The grid is always filled with None.
-#[derive(Component, Reflect, Default, Debug, Clone)]
+#[derive(Component, Reflect, Default, Debug, Clone, FromReflect)]
 #[reflect(Component)]
 pub struct TileStorage {
     tiles: Vec<Option<Entity>>,
