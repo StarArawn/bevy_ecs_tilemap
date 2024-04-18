a12b0fe29646d227b50fe820d4777c287356b776
Uggla <uggla@free.fr>
2024-04-18 22:52:40 +0200

    Add a system to hide tiles.
    Here we can see a bug as only the castle tiles are hidden.

--- examples/tiled.rs
+++ examples/tiled.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 
+use crate::helpers::tiled::{TiledLayersStorage, TiledMap};
+
 mod helpers;
 
 fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -31,5 +33,27 @@ fn main() {
         .add_plugins(helpers::tiled::TiledMapPlugin)
         .add_systems(Startup, startup)
         .add_systems(Update, helpers::camera::movement)
+        .add_systems(Update, hide_tiles)
         .run();
+}
+
+fn hide_tiles(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    tile_storage_query: Query<&TileStorage>,
+    mut map_query: Query<&TiledLayersStorage>,
+    mut tile_query: Query<&mut TileVisible>,
+) {
+    if keyboard_input.pressed(KeyCode::Space) {
+        for layer_storage in map_query.iter_mut() {
+            for layer_entity in layer_storage.storage.values() {
+                if let Ok(layer_tile_storage) = tile_storage_query.get(*layer_entity) {
+                    for tile in layer_tile_storage.iter().flatten() {
+                        if let Ok(mut t) = tile_query.get_mut(*tile) {
+                            t.0 = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
